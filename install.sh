b03ab0a64fc5bd642ac27d1830ab37fceec04cbf
Nathan Lövsund <nathan@lovsund.eu>
2020-08-09 14:14:48 +0200

    Update neovim install script

--- install.sh
+++ install.sh
@@ -11,7 +11,7 @@ fi
 sudo apt update
 sudo apt install -y neovim git curl exuberant-ctags python3-jedi python3-yapf \
 	python3-setproctitle vim-ctrlp vim-airline vim-airline-themes \
-	vim-ctrlp vim-fugitive vim-python-jedi vim-snippets
+	vim-ctrlp vim-fugitive vim-python-jedi vim-snippets global
 	
 curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
 	https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
