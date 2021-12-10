33b56ec93b230f7b808015009ba80336b2803da1
Nathan Lövsund <nathan@lovsund.eu>
2021-12-10 22:13:02 +0100

    Begin migration to nvim 0.5 capabilities

---
+++ home/.config/nvim/init.lua
@@ -0,0 +1,16 @@
+local fn = vim.fn
+
+local install_path = fn.stdpath('data') .. '/site/pack/paqs/start/paq-nvim'
+
+if fn.empty(fn.glob(install_path)) > 0 then
+  fn.system({'git', 'clone', '--depth=1', 'https://github.com/savq/paq-nvim.git', install_path})
+end
+
+require "paq" {
+    "savq/paq-nvim";                  -- Let Paq manage itself
+
+    "neovim/nvim-lspconfig";          -- Mind the semi-colons
+    "hrsh7th/nvim-compe";
+
+    {"lervag/vimtex", opt=true};      -- Use braces when passing options
+}
