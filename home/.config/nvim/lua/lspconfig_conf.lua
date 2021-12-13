5dfec79e5ecf616dba892b5d598f4b6bd584ca8c
Nathan Lövsund <nathan@lovsund.eu>
2021-12-13 20:10:56 +0100

    Add ltex lsp server path

--- home/.config/nvim/lua/lspconfig_conf.lua
+++ home/.config/nvim/lua/lspconfig_conf.lua
@@ -8,8 +8,8 @@ for _, lsp in ipairs(servers) do
   }
 end
 
--- lua lspconfig
 local nvim_data_path = os.getenv("HOME") .. "/.local/share/nvim/lsp_servers/"
+-- lua lspconfig
 nvim_lsp['sumneko_lua'].setup {
   cmd = {  nvim_data_path .. "sumneko_lua/extension/server/bin/Linux/lua-language-server" },
   settings = {
@@ -19,4 +19,9 @@ nvim_lsp['sumneko_lua'].setup {
       }
     }
   }
+}
+
+-- latex & markdown lspconfig
+nvim_lsp['ltex'].setup {
+  cmd = {  nvim_data_path .. "ltex/ltex-ls-15.2.0/bin/ltex-ls" }
 }
