30219afca752911322fa68e74e7e7b8d5bd1db5c
Nathan Lövsund <nathan@lovsund.eu>
2021-12-13 14:25:55 +0100

    Add mappings

---
+++ home/.config/nvim/lua/mappings.lua
@@ -0,0 +1,34 @@
+local vimp = require('vimp')
+
+-- fzf
+vim.api.nvim_set_keymap(
+    'n',
+    '<c-P>',
+    "<cmd>lua require('fzf-lua').files()<CR>", { noremap = true, silent = true }
+)
+
+-- buffer
+vim.api.nvim_set_keymap('', '<F6>', ':ls<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('', '<F7>', ':bprevious<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('', '<F8>', ':bnext<CR>', { noremap = true, silent = true })
+vimp.nnoremap('<leader>q', ':BW<CR>')
+
+-- general
+vimp.nnoremap('<leader>w', ':w<CR>')
+vimp.noremap('<F12>', ':q!<CR>')
+vimp.noremap('<leader>N', ':set number!<CR>')
+vimp.noremap('<leader>R', ':set relativenumber!<CR>')
+vimp.nnoremap('<leader>/', ':nohlsearch<CR>')
+vimp.nnoremap('<C-d>', 'viw')
+vimp.nnoremap('/', '/\\v')
+
+-- insertion
+vimp.inoremap('jk', '<esc>')
+
+-- windows
+vimp.nnoremap('<leader><leader>', '<C-w>w')
+vimp.nnoremap('<leader>v', '<C-w>v')
+vimp.nnoremap('<leader>h', '<C-w>s')
+vimp.nnoremap('<leader>x', '<C-w>q')
+
+-- fugitive
