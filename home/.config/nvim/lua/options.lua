c0070231b843d7675dd055407001e9adc5da0d28
Nathan Lövsund <nathan@lovsund.eu>
2021-12-13 14:25:45 +0100

    Add options

---
+++ home/.config/nvim/lua/options.lua
@@ -0,0 +1,27 @@
+vim.o.title = true
+vim.o.background = "dark"
+vim.o.showcmd = true
+vim.o.showmatch = true
+vim.o.ignorecase = true
+vim.o.smartcase = true
+vim.o.incsearch = true
+vim.o.hlsearch = true
+vim.o.scrolloff = 4
+vim.o.wildmenu = true
+vim.o.wildmode = "full"
+vim.o.number = true
+vim.o.hidden = true
+vim.o.cursorline = true
+vim.o.cursorcolumn = true
+vim.o.colorcolumn = "80,120"
+vim.o.exrc = true
+vim.o.secure = true
+vim.o.encoding = "UTF-8"
+vim.o.tabstop = 8
+vim.o.softtabstop = 4
+vim.o.shiftwidth = 4
+vim.o.expandtab = true
+vim.g.mapleader = " "
+vim.g.maplocalleader = "\\"
+
+require('nightfox').load('nightfox')
