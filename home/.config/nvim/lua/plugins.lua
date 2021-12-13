f0570afcae3ddfe14837199e9419f6467051375e
Nathan Lövsund <nathan@lovsund.eu>
2021-12-13 18:23:06 +0100

    Update plugins
    Re-arrange and add treesitter

--- home/.config/nvim/lua/plugins.lua
+++ home/.config/nvim/lua/plugins.lua
@@ -1,29 +1,48 @@
 require "paq" {
     "savq/paq-nvim";
 
+    -- treesitter
+    "nvim-treesitter/nvim-treesitter";
+
+    -- lsp
     "neovim/nvim-lspconfig";
     "williamboman/nvim-lsp-installer";
 
+    -- completion
     "hrsh7th/cmp-nvim-lsp";
     "hrsh7th/cmp-buffer";
     "hrsh7th/cmp-path";
     "hrsh7th/cmp-cmdline";
-    "hrsh7th/nvim-cmp";
-
     "hrsh7th/cmp-vsnip";
     "hrsh7th/vim-vsnip";
+    "hrsh7th/nvim-cmp";
 
+    -- easy config
     "svermeulen/vimpeccable";
 
+    -- colorscheme
     "EdenEast/nightfox.nvim";
 
+    -- line & fonts
     "nvim-lualine/lualine.nvim";
     "kyazdani42/nvim-web-devicons";
 
+    -- file explorer
     "preservim/nerdtree";
+
+    -- buffer
     "qpkorr/vim-bufkill";
+
+    -- edition
     "tpope/vim-commentary";
-    "airblade/vim-gitgutter";
+
+    -- fuzzy search
     "ibhagwan/fzf-lua"; -- move to telescope with 0.6.0
+
+    -- git
+    "airblade/vim-gitgutter";
     "tpope/vim-fugitive";
+
+    -- register
+    "tversteeg/registers.nvim";
 }
