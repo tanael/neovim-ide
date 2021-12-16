3ed152dc6a6763befd83c1ad0c2e0e35f828de42
Nathan Lövsund <nathan@lovsund.eu>
2021-12-16 19:46:17 +0100

    Manage plugins with packer-nvim
    Instead of paq

--- home/.config/nvim/lua/plugins.lua
+++ home/.config/nvim/lua/plugins.lua
@@ -1,51 +1,74 @@
-require "paq" {
-    "savq/paq-nvim"; -- use packer instead
+local fn = vim.fn
+local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+if fn.empty(fn.glob(install_path)) > 0 then
+  Packer_bootstrap = fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+end
 
-    -- treesitter
-    "nvim-treesitter/nvim-treesitter";
+return require('packer').startup(function(use)
+  -- My plugins here
+  use {
+        'nvim-treesitter/nvim-treesitter',
+        run = ':TSUpdate'
+  }
 
-    -- lsp
-    "neovim/nvim-lspconfig";
-    "williamboman/nvim-lsp-installer";
+  -- lsp
+  use 'neovim/nvim-lspconfig'
+  use 'williamboman/nvim-lsp-installer'
 
-    -- completion
-    "hrsh7th/cmp-nvim-lsp";
-    "hrsh7th/cmp-buffer";
-    "hrsh7th/cmp-path";
-    "hrsh7th/cmp-cmdline";
-    "hrsh7th/cmp-vsnip";
-    "hrsh7th/vim-vsnip";
-    "hrsh7th/nvim-cmp";
+  -- completion
+  use { "hrsh7th/nvim-cmp",
+    requires = {
+      'hrsh7th/vim-vsnip',
+      'hrsh7th/cmp-vsnip',
+      'hrsh7th/cmp-cmdline',
+      'hrsh7th/cmp-path',
+      'hrsh7th/cmp-buffer',
+      'hrsh7th/cmp-nvim-lsp',
+      'neovim/nvim-lspconfig'
+    }
+  }
 
-    -- easy config
-    "svermeulen/vimpeccable";
+  -- easy config
+  use "svermeulen/vimpeccable"
 
-    -- colorscheme
-    "EdenEast/nightfox.nvim";
+  -- colorscheme
+  use "EdenEast/nightfox.nvim"
 
-    -- line & fonts
-    "nvim-lualine/lualine.nvim";
-    "kyazdani42/nvim-web-devicons";
+  -- line & fonts
+  use { "nvim-lualine/lualine.nvim",
+    requires = "kyazdani42/nvim-web-devicons"
+  }
 
-    -- file explorer
-    "kyazdani42/nvim-tree.lua";
+  -- file explorer
+  use "kyazdani42/nvim-tree.lua";
 
-    -- buffer
-    "qpkorr/vim-bufkill";
+  -- buffer
+  use "qpkorr/vim-bufkill";
 
-    -- edition
-    "tpope/vim-commentary";
+  -- edition
+  use "tpope/vim-commentary";
 
-    -- fuzzy search
-    "ibhagwan/fzf-lua"; -- move to telescope with 0.6.0
+  -- fuzzy search
+  use "ibhagwan/fzf-lua"; -- move to telescope with 0.6.0
 
-    -- git
-    "airblade/vim-gitgutter";
-    "tpope/vim-fugitive";
+  -- git
+  use "airblade/vim-gitgutter"
+  use "tpope/vim-fugitive"
 
-    -- register
-    "tversteeg/registers.nvim";
+  -- register
+  use "tversteeg/registers.nvim"
 
-    -- diagnostics
-    "folke/trouble.nvim";
-}
+  -- diagnostics
+  use { "folke/trouble.nvim",
+    requires = "kyazdani42/nvim-web-devicons",
+    config = function()
+      require("trouble").setup {}
+    end
+  }
+
+  -- Keep this at the end
+  -- Automatically set up my configuration after cloning packer.nvim
+  if Packer_bootstrap then
+    require('packer').sync()
+  end
+end)
