cc49b5ec7dd65f09df4402596b92d589e5eb683e
Nathan Lövsund <nathan@lovsund.eu>
2021-12-18 19:15:24 +0100

    Add nvim-autopairs

--- home/.config/nvim/lua/cmp_conf.lua
+++ home/.config/nvim/lua/cmp_conf.lua
@@ -46,3 +46,7 @@ cmp.setup.cmdline(':', {
     { name = 'cmdline' }
   })
 })
+
+-- autopairs
+local cmp_autopairs = require('nvim-autopairs.completion.cmp')
+cmp.event:on( 'confirm_done', cmp_autopairs.on_confirm_done({  map_char = { tex = '' } }))
