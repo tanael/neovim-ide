580772567ac404478b207ad7a228c7fa82367a15
Nathan Lövsund <nathan@lovsund.eu>
2021-12-18 16:02:07 +0100

    Add coloreschemes

--- home/.config/nvim/lua/colorscheme_conf.lua
+++ home/.config/nvim/lua/colorscheme_conf.lua
@@ -1,1 +1,13 @@
+-- nightfox
 require('nightfox').load('nightfox')
+
+-- onedark
+-- vim.g.onedark_style = 'deep'
+-- require('onedark').setup()
+
+-- material
+-- vim.g.material_style = 'darker'
+-- require('material').setup()
+-- vim.cmd 'colorscheme material'
+-- require('material.functions').toggle_style()
+-- vim.api.nvim_set_keymap('n', '<leader>mm', [[<Cmd>lua require('material.functions').toggle_style()<CR>]], { noremap = true, silent = true })
