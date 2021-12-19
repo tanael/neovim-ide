76ffd999d4d373a2397261ad79e6af1713bfa46e
Nathan Lövsund <nathan@lovsund.eu>
2021-12-19 12:26:47 +0100

    Update whichkey_conf
    Trouble and Todo mappings

--- home/.config/nvim/lua/whichkey_conf.lua
+++ home/.config/nvim/lua/whichkey_conf.lua
@@ -2,7 +2,11 @@ local wk = require('whichkey_setup')
 
 local keymap = {
     t = {':NvimTreeToggle<CR>', 'Toggle File Explorer'},
-    T = {':TroubleToggle<CR>', 'Toggle Diagnostics Window'},
+    d = {
+      name = '+diagnostics',
+      t = {':TroubleToggle<CR>', 'Toggle Diagnostics Window'},
+      T = {':TodoTrouble<CR>', "Show project's TODOs and such inside trouble"},
+    }
 }
 
 wk.register_keymap('leader', keymap, { noremap=true, silent=true })
