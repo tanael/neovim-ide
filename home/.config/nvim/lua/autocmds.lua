544d6a5699a830f2ea5f7be747759791676e2ca8
Nathan Lövsund <nathan@lovsund.eu>
2022-01-22 20:25:46 +0100

    Add markdown autocmd

--- home/.config/nvim/lua/autocmds.lua
+++ home/.config/nvim/lua/autocmds.lua
@@ -3,4 +3,8 @@ vim.cmd([[
       autocmd!
       autocmd FileType lua setlocal tabstop=8 expandtab shiftwidth=2 softtabstop=2
   augroup end
+  augroup markdown
+      autocmd!
+      autocmd FileType markdown setlocal tabstop=8 expandtab shiftwidth=2 softtabstop=2
+  augroup end
 ]])
