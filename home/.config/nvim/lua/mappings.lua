63bf2a828beb89b3ffbb8f1fd28e850539ffd973
Nathan Lövsund <nathan@lovsund.eu>
2022-11-29 14:08:39 +0100

    Add Telescope mappings

--- home/.config/nvim/lua/mappings.lua
+++ home/.config/nvim/lua/mappings.lua
@@ -61,3 +61,10 @@ vimp.tnoremap('<A-l>', '<C-\\><C-N><C-w>l')
 
 -- fugitive
 vimp.noremap('<leader>gs', ':Git<CR>')
+
+-- telescope
+local builtin = require('telescope.builtin')
+vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
+vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
+vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
