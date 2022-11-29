9980d082307ed2b7e5ca28440c534715d60cb963
Nathan Lövsund <nathan@lovsund.eu>
2022-11-29 16:15:21 +0100

    Refactor LSP configuration
    With key bindings on_attach.

--- home/.config/nvim/lua/lspconfig_conf.lua
+++ home/.config/nvim/lua/lspconfig_conf.lua
@@ -1,11 +1,39 @@
 -- Setup lspconfig.
 local capabilities = require('cmp_nvim_lsp').default_capabilities(vim.lsp.protocol.make_client_capabilities())
 local nvim_lsp = require('lspconfig')
-local servers = { 'bashls', 'ltex', 'sumneko_lua', 'pylsp', 'rust_analyzer' }
-for _, lsp in ipairs(servers) do
-  nvim_lsp[lsp].setup {
-    capabilities = capabilities,
-  }
+
+-- Mappings.
+-- See `:help vim.diagnostic.*` for documentation on any of the below functions
+local opts = { noremap=true, silent=true }
+vim.keymap.set('n', '<space>e', vim.diagnostic.open_float, opts)
+vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, opts)
+vim.keymap.set('n', ']d', vim.diagnostic.goto_next, opts)
+vim.keymap.set('n', '<space>Q', vim.diagnostic.setloclist, opts)
+
+-- Use an on_attach function to only map the following keys
+-- after the language server attaches to the current buffer
+local on_attach = function(client, bufnr)
+  -- Enable completion triggered by <c-x><c-o>
+  vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
+
+  -- Mappings.
+  -- See `:help vim.lsp.*` for documentation on any of the below functions
+  local bufopts = { noremap=true, silent=true, buffer=bufnr }
+  vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, bufopts)
+  vim.keymap.set('n', 'gd', vim.lsp.buf.definition, bufopts)
+  vim.keymap.set('n', 'K', vim.lsp.buf.hover, bufopts)
+  vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, bufopts)
+  vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, bufopts)
+  vim.keymap.set('n', '<space>wa', vim.lsp.buf.add_workspace_folder, bufopts)
+  vim.keymap.set('n', '<space>wr', vim.lsp.buf.remove_workspace_folder, bufopts)
+  vim.keymap.set('n', '<space>wl', function()
+    print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+  end, bufopts)
+  vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, bufopts)
+  vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, bufopts)
+  vim.keymap.set('n', '<space>ca', vim.lsp.buf.code_action, bufopts)
+  vim.keymap.set('n', 'gr', vim.lsp.buf.references, bufopts)
+  vim.keymap.set('n', '<space>f', function() vim.lsp.buf.format { async = true } end, bufopts)
 end
 
 local nvim_data_path = os.getenv("HOME") .. "/.local/share/nvim/lsp_servers/"
@@ -13,6 +41,7 @@ local nvim_data_path = os.getenv("HOME") .. "/.local/share/nvim/lsp_servers/"
 nvim_lsp['sumneko_lua'].setup {
   cmd = {  nvim_data_path .. "sumneko_lua/extension/server/bin/lua-language-server" },
   capabilities = capabilities,
+  on_attach = on_attach,
   settings = {
     Lua = {
       diagnostics = {
@@ -26,6 +55,7 @@ nvim_lsp['sumneko_lua'].setup {
 nvim_lsp['ltex'].setup {
   cmd = {  nvim_data_path .. "ltex/ltex-ls/bin/ltex-ls" },
   capabilities = capabilities,
+  on_attach = on_attach,
   settings = {
     ltex = {
       disabledRules = { ['en-US'] = { 'PROFANITY' } },
@@ -43,6 +73,7 @@ nvim_lsp['ltex'].setup {
 nvim_lsp['pylsp'].setup {
   cmd = { 'pylsp' }, -- install python3-pylsp-mypy
   capabilities = capabilities,
+  on_attach = on_attach,
 }
 
 -- html
@@ -51,6 +82,7 @@ html_capabilities.textDocument.completion.completionItem.snippetSupport = true
 
 nvim_lsp['html'].setup{
   capabilities = html_capabilities,
+  on_attach = on_attach,
 }
 
 -- haskell
@@ -58,6 +90,7 @@ local util = require 'lspconfig.util'
 nvim_lsp['hls'].setup{
   cmd = { "haskell-language-server-wrapper", "--lsp" },
   capabilities = capabilities,
+  on_attach = on_attach,
   filetypes = { "haskell", "lhaskell" },
   root_dir = function (filepath)
       return (
